348c5b65a38bf8752fe2f22042a862c30459bc4c
alebreux <alexandre.lebreux@gmail.com>
2018-12-01 17:43:22 +0100

    gp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sccs']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'iot-front';
+
+  constructor( private http: HttpClient){
+
+  };
+
+
+  test(): void {
+    this.http.get(environment.url + '/').subscribe();
+  }
+  lumiere(numero: number): void {
+    this.http.get(environment.url + '/lumiere/allume/' +numero).subscribe();
+}
+
 }
